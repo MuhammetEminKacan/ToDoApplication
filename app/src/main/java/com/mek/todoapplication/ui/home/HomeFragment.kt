919ff17dd867e9c80ec2c8506987ecb083ce0ab1
MuhammetEminKacan <kacanmuhammetemin73@gmail.com>
2025-03-25 18:27:30 +0300

    adapter created and bindingAdapter added

--- app/src/main/java/com/mek/todoapplication/ui/home/HomeFragment.kt
+++ app/src/main/java/com/mek/todoapplication/ui/home/HomeFragment.kt
@@ -1,18 +1,18 @@
 package com.mek.todoapplication.ui.home
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import com.mek.todoapplication.R
 import com.mek.todoapplication.databinding.FragmentHomeBinding
+import com.mek.todoapplication.model.ToDoModel
 import dagger.hilt.android.AndroidEntryPoint
 
+
 @AndroidEntryPoint
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(),ToDoClickListener {
     private var _binding : FragmentHomeBinding ?= null
     private val binding get() =  _binding!!
 
@@ -35,6 +35,10 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModel = viewModel
+        binding.toDoClickListener = this
+
         viewModel.toDoList.observe(viewLifecycleOwner){
             println(it)
         }
@@ -48,5 +52,13 @@ class HomeFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onToDoClick(id: Int) {
+
+    }
+
+    override fun onToDoChecked(toDoModel: ToDoModel) {
+
     }
 }
